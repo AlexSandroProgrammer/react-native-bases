7f3714e1c0521aab93e597afcdd3a94bc581a9dc
alexsandro2005 <luisalejandrm533@gmail.com>
2024-12-25 13:20:21 -0500

    feat: add config tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import "./App.css";
 // import BasicFunction from "./typescript/BasicFunction";
 // import BasicTypes from "./typescript/BasicTypes";
 // import ObjectLiterals from "./typescript/ObjectLiterals";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <>
-      <h1>HELLO WORLD</h1>
+      <h1 className="text-3xl font-bold underline">HELLO WORLD</h1>
 
       {/* <BasicTypes />
       <ObjectLiterals /> */}
       {/* <BasicFunction /> */}
+      <Counter />
     </>
   );
 }
